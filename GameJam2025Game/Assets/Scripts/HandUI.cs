9ae3abea8ba875f3af22866b0ecd4adc438b8bab
tom-motiejunas <tom.motiejunas@gmail.com>
2025-01-25 18:24:49 +0200

    Updated handUI

--- GameJam2025Game/Assets/Scripts/HandUI.cs
+++ GameJam2025Game/Assets/Scripts/HandUI.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HandUI : MonoBehaviour
 {
     [SerializeField] private Player _player = null;
+
+    void Start()
+    {
+        this.gameObject.SetActive(false);
+    }
 
 public void onChangeHandUi(Sprite newHandImage)
 {
@@ -14,9 +20,11 @@ public void onChangeHandUi(Sprite newHandImage)
     if (newHandImage)
     {
         image.sprite = newHandImage;
+        this.gameObject.SetActive(true);
     }
     else
     {
+        this.gameObject.SetActive(false);
         Debug.LogWarning("SpriteRenderer component not found on the GameObject.");
     }
 }
